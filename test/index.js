879db425c2a4f36327f2a22d9bb407b635510b04
Peiwen Lu <hi@peiwen.lu>
2020-04-04 10:27:08 +0800

    Add helper functions

--- test/index.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-const sass = require("sass");
-const sassTrue = require("sass-true");
-
-sassTrue.runSass(
-  { file: path.resolve(__dirname, "scss/test.scss") },
-  { sass, describe, it }
-);
