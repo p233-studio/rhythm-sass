2d0c2e968eb7095718dacb33f3a71df99cefd82f
Peiwen Lu <hi@peiwen.lu>
2023-06-10 09:26:58 +0800

    Refactor

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,5 +1,5 @@
 const path = require("path");
 const sassTrue = require("sass-true");
 
-sassTrue.runSass({ file: path.resolve(__dirname, "core.test.scss") }, { describe, it });
-sassTrue.runSass({ file: path.resolve(__dirname, "rhythm.test.scss") }, { describe, it });
+sassTrue.runSass({ describe, it }, path.resolve(__dirname, "lib.test.scss"));
+sassTrue.runSass({ describe, it }, path.resolve(__dirname, "rhythm.test.scss"));
